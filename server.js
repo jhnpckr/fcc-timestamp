94fcade55eb3ae18d72c33349f458e0648d09097
John Packer <johnpacker100@gmail.com>
2016-02-20 16:47:12 +0000

    update log message

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.get('*', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Listening on port 3000...');
+    console.log('Listening on port ' + (process.env.PORT ? process.env.PORT : '3000') + '...');
 });
